a06afdb18b10bd9e262af505dc26432aed1acab9
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-04-25 16:34:31 +0300

    Mutex örneği yapıldı

--- philosophers.c
+++ philosophers.c
@@ -6,7 +6,39 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:45:57 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/04/21 23:45:59 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/04/25 02:53:04 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
+int counter = 0;
+
+void* print_hello(void* arg) 
+{
+    pthread_mutex_t *lock = (pthread_mutex_t *)arg;
+
+    pthread_mutex_lock(lock);
+    
+    for (int i = 0; i < 5555; i++) 
+    {
+        counter++;
+    }
+    pthread_mutex_unlock(lock);
+    return NULL;
+}
+
+int main() 
+{
+    pthread_t t1, t2;
+    pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    pthread_create(&t1, NULL, print_hello, &my_mutex);
+    pthread_create(&t2, NULL, print_hello,  &my_mutex);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    
+    printf("counter : %d\n", counter);
+    return 0;
+}
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,18 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:46:00 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/04/21 23:46:01 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/04/22 22:38:55 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#endif
