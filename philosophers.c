83e3dd0ffcd71561e708c7e25050fe73679e3cc6
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-05-06 01:16:36 +0300

    main thread ile yeni thread'in çakışması sorunu test edildi

--- philosophers.c
+++ philosophers.c
@@ -6,38 +6,20 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 23:45:57 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/05/05 01:23:10 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/05/06 01:15:00 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void *print(void *arg)
+{
+    printf("Merhaba ben Thread\n");
+    return NULL;
+}
 int main() 
 {
-    struct timeval start;
-    struct timeval stop;
-    long milisecond;
-
-    gettimeofday(&start, NULL);
-    for (int i = 0; i < 3000000; i++)
-    {
-        printf("\n");
-    }
-    gettimeofday(&stop, NULL);
-
-    milisecond = (stop.tv_sec*1000 + stop.tv_usec / 1000) - (start.tv_sec*1000 + start.tv_usec / 1000);
-    printf("3 Milyon for işlemi için Geçen toplam milisaniye : %ld\n", milisecond);
-    usleep(3000000);
-    int j = 0;    
-    gettimeofday(&start, NULL);
-    while (j < 3000000)
-    {
-        printf("\n");
-        j++;
-    }
-    gettimeofday(&stop, NULL);
-
-    milisecond = (stop.tv_sec*1000 + stop.tv_usec / 1000) - (start.tv_sec*1000 + start.tv_usec / 1000);
-    printf("3 Milyon while işlemi için Geçen toplam milisaniye : %ld\n", milisecond);
-    
+    pthread_t thread;
+    pthread_create(&thread, NULL, print, NULL);
+    printf("Burası main thread\n");
 }
