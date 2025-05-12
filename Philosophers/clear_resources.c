01c2e5ef9f5ac966f7ecb890c89fcb5a83cafc99
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-05-13 00:16:24 +0300

    free_resources ikiye bölündü destroy_mutex yazıldı

--- Philosophers/clear_resources.c
+++ Philosophers/clear_resources.c
@@ -6,17 +6,33 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 18:28:54 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/05/12 23:48:19 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/05/13 00:15:21 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void	free_resources(t_params *params, t_philo *philos, bool is_init_philo)
+static	void	destroy_mutex(t_philo *philos, t_params *params)
 {
 	int	i;
 
-	i = -1;
+	i = 0;
+	while (philos != NULL && i < params->philo_count && philos[i].meal_mutex != NULL)
+	{
+		pthread_mutex_destroy(philos[i].meal_mutex);
+		free(philos[i].meal_mutex);
+		i++;
+	}
+	i = 0;
+	while (params != NULL && i < params->philo_count)
+	{
+		pthread_mutex_destroy(&params->forks[i]);
+		i++;
+	}
+}
+
+void	free_resources(t_params *params, t_philo *philos, bool is_init_philo)
+{
 	if (params != NULL && params->print_mutex != NULL)
 	{
 		pthread_mutex_destroy(params->print_mutex);
@@ -24,15 +40,8 @@ void	free_resources(t_params *params, t_philo *philos, bool is_init_philo)
 	}
 	if (philos != NULL)
 	{
-		while (++i < params->philo_count && is_init_philo)
-		{
-			if (philos[i].meal_mutex != NULL)
-			{
-				pthread_mutex_destroy(philos[i].meal_mutex);
-				free(philos[i].meal_mutex);
-			}
-			pthread_mutex_destroy(philos[i].left_fork);
-		}
+		if(is_init_philo)
+			destroy_mutex(philos, params);
 		free(philos);
 	}
 	if (params != NULL && params->forks != NULL)
