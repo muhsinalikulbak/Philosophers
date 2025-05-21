48b156a2579720a40afd0891c6bb347b2347a4e0
Muhsin <muhsin.kulbak@gmail.com>
2025-05-21 03:07:09 +0300

    Rutin fonksiyonu ve 1 filozof kontrolü yazıldı

--- Philosophers/simulation.c
+++ Philosophers/simulation.c
@@ -6,11 +6,21 @@
 /*   By: muhsin <muhsin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 00:42:37 by muhsin            #+#    #+#             */
-/*   Updated: 2025/05/21 02:33:42 by muhsin           ###   ########.fr       */
+/*   Updated: 2025/05/21 03:06:55 by muhsin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+static bool	is_sim_ended(t_philo *philo)
+{
+	bool	status;
+	
+	pthread_mutex_lock(philo->params->print_mutex);
+	status = philo->params->sim_end;
+	pthread_mutex_unlock(philo->params->print_mutex);
+	return (status);
+}
 
 static void	*philosopher_routine(void *arg)
 {
@@ -19,7 +29,15 @@ static void	*philosopher_routine(void *arg)
 	pthread_mutex_lock(philo->meal_mutex);
 	philo->last_meal_time = get_current_time();
 	pthread_mutex_unlock(philo->meal_mutex);
-	while (is_sim_ended())
+	if (philo->params->philo_count == 1)
+	{
+		pthread_mutex_lock(philo->left_fork);
+		print_status(philo, get_current_time(), "has taken a fork");
+		accurate_sleep(philo->params->time_to_die);
+		pthread_mutex_unlock(philo->left_fork);
+		return (NULL);
+	}
+	while (is_sim_ended(philo))
 	{
 		take_forks(philo);
 		eat(philo);
@@ -30,7 +48,7 @@ static void	*philosopher_routine(void *arg)
 	return (NULL);
 }
 
-static void	create_thread(t_philo *philos)
+static bool	create_thread(t_philo *philos)
 {
 	int	i;
 
@@ -40,7 +58,7 @@ static void	create_thread(t_philo *philos)
 		if (pthread_create(&philos[i].thread, NULL, philosopher_routine, &philos[i]) != 0)
 		{
 			error_manage(STDERR, philos->params, philos);
-			return ;
+			return (false);
 		}
 		i++;
 	}
@@ -58,12 +76,8 @@ static void	wait_thread(t_philo *philos)
 	}
 }
 
-static bool	is_sim_ended()
-{
-	
-}
 
-void	start_simulation(t_philo *philos)
+bool start_simulation(t_philo *philos)
 {
 	create_thread(philos);
 	wait_thread(philos);
